3acc0d921a6f85301207ff3970f78de6c6d0201f
abhishek.modi <abhishek.modi@nihilent.com>
2018-04-11 11:37:08 +0530

    Adding the Hockey app integratin into the Circle CI

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -7,4 +7,4 @@ curl \
 -F "notify=0" \
 -F "ipa=@AppName/build/outputs/apk/appname-dev-debug.apk" \
 -H "X-HockeyAppToken: $HockeyAppToken" \
-https://rink.hockeyapp.net/api/2/apps/$AndroidAppId_Dev/app_versions/upload
+https://rink.hockeyapp.net/api/2/apps/$AndroidAppId/app_versions/upload
